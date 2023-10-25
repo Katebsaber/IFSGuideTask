ad0b76644eaee07cd289cb5745873e6033ad11cd
Mohamad Amin Katebsaber <katebsaber96@gmail.com>
2023-10-25 09:52:03 +0330

    chore: reformat crud

--- src/dialogue_manager/crud.py
+++ src/dialogue_manager/crud.py
@@ -23,9 +23,8 @@ def get_user_dialogue_ids(db: Session, user_id: str, skip: int = 0, limit: int =
         .all()
     )
 
-def get_all_dialogue_messages(
-    db: Session, dialogue_id: str, user_id: str
-):
+
+def get_all_dialogue_messages(db: Session, dialogue_id: str, user_id: str):
     return (
         db.query(DBMessage)
         .filter(DBMessage.dialogue_id == dialogue_id)
@@ -33,6 +32,7 @@ def get_all_dialogue_messages(
         .order_by(DBMessage.created_at)
         .all()
     )
+
 
 def get_dialogue_messages(
     db: Session, dialogue_id: str, user_id: str, skip: int = 0, limit: int = 10
@@ -66,14 +66,15 @@ def create_message(db: Session, message: SchemaMessage):
 if __name__ == "__main__":
     from datetime import datetime, timezone
     from schemas import SchemaMessageType
+
     msg = SchemaMessage(
-        id = "test_msg_id",
+        id="test_msg_id",
         user_id="test_user",
         dialogue_id="test_dilg_id",
         created_at=datetime.now(timezone.utc),
         content="hello",
         message_type=SchemaMessageType.human,
-        in_response_to=None
+        in_response_to=None,
     )
     # print(msg)
     # print(msg.user_id)
@@ -85,4 +86,12 @@ if __name__ == "__main__":
     db = SessionLocal()
     # create_message(db=db, message=msg)
 
-    print(get_user_dialogue_ids(db, user_id="c85f5866-852e-40d8-a4f2-2dada07c7708"))
+    print(
+        get_dialogue_messages(
+            db=db,
+            dialogue_id="b69dfb2e-e1c9-48f0-879a-f1243c0d58ef",
+            user_id="c85f5866-852e-40d8-a4f2-2dada07c7708",
+            skip=0,
+            limit=10,
+        )
+    )
